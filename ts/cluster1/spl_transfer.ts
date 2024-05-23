ff7b41a40ec39c9c35fe2c02cc9845fcfdf9e974
santoshimurakami <santoshimurakami@gmail.com>
2024-05-23 20:13:40 +0800

    spl token done

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -2,6 +2,8 @@ import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@s
 import wallet from "../wba-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
+const token_decimals = 1000000n;
+
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
@@ -10,18 +12,23 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("9fZDajxYKx1imnMUAMAWrpTyiH1ELiifwDLJo9i7V4qp");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("7NMkatoB4FiVU3YQzPXeN7FyXCt9CUT8ruXDtoW3opzT");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        let fromTokenAccount = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        let toTokenAccount = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to);
 
         // Transfer the new token to the "toTokenAccount" we just created
+        let tx_hash = await transfer(connection, keypair, fromTokenAccount.address, toTokenAccount.address, keypair.publicKey, 10n * token_decimals);
+
+        console.log("Transaction hash is: ", tx_hash);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
